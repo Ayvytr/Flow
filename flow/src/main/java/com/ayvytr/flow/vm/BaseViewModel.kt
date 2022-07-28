6029ec5a4fa71813dea392e98e01c4bdf3c04eaf
wangdunwei <ayvytr@163.com>
2022-07-28 11:33:45 +0800

    删除ErrorObserver

--- flow/src/main/java/com/ayvytr/flow/vm/BaseViewModel.kt
+++ flow/src/main/java/com/ayvytr/flow/vm/BaseViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import com.ayvytr.flow.BaseConfig
 import com.ayvytr.flow.base.IView
 import com.ayvytr.flow.exception.NetworkException
-import com.ayvytr.flow.observer.ErrorObserver
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import java.util.*
@@ -42,9 +41,9 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
      * [request.javaClass.name]:请求签名，类似com.ayvytr.coroutines.main.MainViewModel$getAndroidPostFlow$1
      *
      * 注意：
-     *  如果实现了[onError]，就不会调用[IView.showMessage]，需要重写并保留错误提示请使用[ErrorObserver]
+     *  如果实现了[onError]，就不会调用[IView.showMessage]
      *  retry的逻辑不能放到[flowOn]之后[collect]之前，不然不生效
-     *  两个[flowOn]中间的代码不能抽取方法，不然异常捕获不到
+     *  [launchFlow]的两个[flowOn]中间的代码不能抽取方法和[zipFlow]公用，不然[catch]不到异常
      */
     fun <T> launchFlow(
         request: suspend () -> T,
@@ -52,7 +51,7 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
         showLoading: Boolean = true,
         retry: Boolean = false,
         repeatSameJob: Boolean = false,
-        onError: ((NetworkException) -> Unit)? = null
+        onError: ((NetworkException) -> Unit)? = { view.showMessage(it.stringId) }
     ) {
         //requestKey代表调用到当前位置，毫秒数代表具体的某一次调用
         val requestKey = request.javaClass.name
@@ -71,7 +70,7 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
                 .onStart {
                     if (showLoading) {
                         //显示loading
-                        view?.showLoading()
+                        view.showLoading()
                     }
                 }
             if (retry) {
@@ -94,7 +93,7 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
                 //如果没有正在loading的接口，隐藏loading
                 val isEmpty = jobList.filter { it.second }.isEmpty()
                 if (isEmpty) {
-                    view?.showLoading(false)
+                    view.showLoading(false)
                 }
             }
         }
@@ -102,92 +101,22 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
 
 
     /**
-     * [launch]和[flow]实际在这里执行.
-     *
-     * [showLoading]：是否显示loading（比如LoadingDialog)，默认true
-     * [retry]: 接口返回错误后是否重试，默认false
-     * [repeatSameJob]: 是否重复请求相同签名[request.javaClass.name]的接口，默认false
-     * [onError]: 默认显示toast. 注意重载：
-     *      object: ErrorObserver {} 是方便重写的，可保留原错误提示并增加更多内容
-     *      ((NetworkException) -> Unit)? 是方便覆盖的，如果不为空，覆盖默认错误显示逻辑
-     *
-     * onCompletion比invokeOnCompletion先调用
-     * JobCancellationException走onCompletion()，不走catch()
-     * [request.javaClass.name]:请求签名，类似com.ayvytr.coroutines.main.MainViewModel$getAndroidPostFlow$1
-     *
-     * 注意：
-     *  retry的逻辑不能放到[flowOn]之后[collect]之前，不然不生效
-     *  两个[flowOn]中间的代码不能抽取方法，不然异常捕获不到
-     *
+     * 通过[transform],合并[request1]请求的[T]和[request2]请求的[R]，[onSuccess]返回[OUT]
+     * @see [launchFlow]
      */
-//    @JvmOverloads
-//    fun <T> launchFlow(
-//        request: suspend () -> T,
-//        onSuccess: (T) -> Unit,
-//        showLoading: Boolean = true,
-//        retry: Boolean = false,
-//        repeatSameJob: Boolean = false,
-//        onError: ErrorObserver? = object: ErrorObserver {}
-//    ) {
-//        //requestKey代表调用到当前位置，毫秒数代表具体的某一次调用
-//        val requestKey = request.javaClass.name
-//        val key = "$requestKey:${System.currentTimeMillis()}"
-//
-//        synchronized(this) {
-//            if (repeatSameJob && jobList.any { it.first.contains(requestKey) }) {
-//                return
-//            }
-//        }
-//
-//        val job = launch {
-//            var flow = flow<T> {
-//                emit(request.invoke())
-//            }.flowOn(Dispatchers.IO)
-//                .onStart {
-//                    if (showLoading) {
-//                        //显示loading
-//                        view?.showLoading()
-//                    }
-//                }
-//            if (retry) {
-//                flow = flow.retry(BaseConfig.networkRetryCount.toLong())
-//            }
-//            flow.catch {
-//                onError?.onError(view, BaseConfig.networkExceptionConverter.invoke(it))
-//            }.flowOn(Dispatchers.Main)
-//                .collect {
-//                    onSuccess.invoke(it)
-//                }
-//        }
-//
-//        addJob(key, showLoading, job)
-//
-//        job.invokeOnCompletion {
-//            removeJobByKey(key)
-//
-//            if (showLoading) {
-//                //如果没有正在loading的接口，隐藏loading
-//                val isEmpty = jobList.filter { it.second }.isEmpty()
-//                if (isEmpty) {
-//                    view?.showLoading(false)
-//                }
-//            }
-//        }
-//    }
-
     @JvmOverloads
     fun <T, R, OUT> zipFlow(
-        r1: suspend () -> T,
-        r2: suspend () -> R,
+        request1: suspend () -> T,
+        request2: suspend () -> R,
         transform: (T, R) -> OUT,
         onSuccess: (OUT) -> Unit,
         showLoading: Boolean = true,
         retry: Boolean = false,
         repeatSameJob: Boolean = false,
-        onError: ErrorObserver? = object: ErrorObserver {}
+        onError: ((NetworkException) -> Unit)? = { view.showMessage(it.stringId) }
     ) {
         //requestKey代表调用到当前位置，毫秒数代表具体的某一次调用
-        val requestKey = r1.javaClass.name + "," +  r2.javaClass.name
+        val requestKey = request1.javaClass.name + "," + request2.javaClass.name
         val key = "$requestKey:${System.currentTimeMillis()}"
 
         synchronized(this) {
@@ -201,19 +130,19 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
                 //async{}.await()问题：catch不到404异常
 //                val v1 = async { r1.invoke() }.await()
 //                val v2 = async { r2.invoke() }.await()
-                emit(transform(r1.invoke(), r2.invoke()))
+                emit(transform(request1.invoke(), request2.invoke()))
             }.flowOn(Dispatchers.IO)
                 .onStart {
                     if (showLoading) {
                         //显示loading
-                        view?.showLoading()
+                        view.showLoading()
                     }
                 }
             if (retry) {
                 flow = flow.retry(BaseConfig.networkRetryCount.toLong())
             }
             flow.catch {
-                onError?.onError(view, BaseConfig.networkExceptionConverter.invoke(it))
+                onError?.invoke(BaseConfig.networkExceptionConverter.invoke(it))
             }.flowOn(Dispatchers.Main)
                 .collect {
                     onSuccess.invoke(it)
@@ -229,7 +158,7 @@ open class BaseViewModel<out V: IView> : ViewModel(), CoroutineScope by MainScop
                 //如果没有正在loading的接口，隐藏loading
                 val isEmpty = jobList.filter { it.second }.isEmpty()
                 if (isEmpty) {
-                    view?.showLoading(false)
+                    view.showLoading(false)
                 }
             }
         }
